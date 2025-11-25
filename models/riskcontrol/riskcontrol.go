b446cfce1f4fe2883816368c35f1cf610d37ebca
bayugrafit <bayu.alfian@ag-it.com>
2025-11-26 00:59:42 +0700

    refac(): add new field to declare model risk control

--- models/riskcontrol/riskcontrol.go
+++ models/riskcontrol/riskcontrol.go
@@ -4,7 +4,14 @@ type RiskControl struct {
 	ID          int64
 	Kode        string
 	RiskControl string
+	ControlType string
+	Nature      string
+	KeyControl  string
 	Deskripsi   string
+	OwnerLvl    string
+	OwnerGroup  string
+	Owner       string
+	Document    string
 	Status      bool
 	CreatedAt   *string
 	UpdatedAt   *string
